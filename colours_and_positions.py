a9fbb0552898c3920d5595339cdd3823b608cb89
David Cantidio <dbcantidio@gmail.com>
2022-12-30 16:55:17 -0300

    broke code in smaller functions

--- colours_and_positions.py
+++ colours_and_positions.py
@@ -1,4 +1,85 @@
+import pyautogui as pt
+from time import sleep
 from datetime import datetime
 
 datetime.now()
 time_stamp = datetime.strftime(datetime.now(), "%d-%m-%y _ %H:%M")
+file_name = time_stamp + ".png"
+
+sleep(2)
+
+def start_point():
+    start_point = pt.locateCenterOnScreen("start_point.png", confidence=.8)
+    pt.moveTo(start_point)
+    pt.moveRel(0,250)
+    pt.click()
+
+def dl_audio():
+    audios = pt.locateAllOnScreen("new_audio.png", confidence=.8)
+    options = pt.locateAllOnScreen("options.png", confidence=.5)
+    
+    sleep(2)
+    if audios is not None:
+        print('tem audio')
+        *_, new_audio = audios
+        pt.moveTo(new_audio[0], new_audio[1])
+        sleep(2)
+        pt.moveRel(230, 0)
+        pt.click()
+        sleep(1)
+        dl = pt.locateCenterOnScreen("download.png", confidence=.8)
+        if dl is not None:
+            pt.moveTo(dl[0], dl[1])
+            pt.click()
+            start_point()
+        else:
+            print("não achei botão de download")
+
+      
+   
+
+
+# try:
+#     #check if new message is an audio message
+#     play_button = pt.locateCenterOnScreen("play.png", confidence=.8 )
+#     if play_button:
+#         print("tem audio")
+#         pt.moveTo(play_button)
+#         options = pt.locateCenterOnScreen("options.png", confidence=.8 )
+#         pt.moveTo(options)
+#         pt.click()
+#         # pt.moveRel(0, -180)
+#         sleep(5)
+#                 # pt.click()
+
+#                     # sleep(1)
+#                     # pt.moveRel(0, 180)
+#                     # sleep(1)
+#                     # pt.click()
+#                     # sleep(1)
+
+#                     # pt.moveRel(10, -45)
+#                     # sleep(1)
+#                     # pt.click()
+
+#                     # sleep(1)
+#                     # pt.moveRel(0, -50)
+#                     # pt.click()
+#                     # sleep(1)
+
+#                     # sleep(1)
+#                     # pt.moveRel(-400, 0)
+#                     # pt.click()
+#                     # sleep(1)
+#                 else:
+#                     start_point()
+#             except:
+#                 print("no audio sent")
+#                 start_point()
+
+#     except:
+#         start_point()
+#         print("no new messages")
+
+
+dl_audio()
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pyautogui as pt
 from time import sleep
-import pyperclip
 from datetime import datetime
 
 datetime.now()
@@ -8,53 +7,53 @@ time_stamp = datetime.strftime(datetime.now(), "%d-%m-%y _ %H:%M")
 file_name = time_stamp + ".png"
 
 sleep(2)
+def start_point():
+    start_point = pt.locateCenterOnScreen("start_point.png", confidence=.8)
+    pt.moveTo(start_point)
+    pt.moveRel(0,250)
+    pt.click()
 
 
-def check_new_messages():
-    green_button = pt.locateOnScreen("new.png", confidence=.8)
+def dl_audio():
+    
+    audios = pt.locateAllOnScreen("new_audio.png", confidence=.8)
+    if audios is not None:
+        print('tem audio')
+        *_, new_audio = audios
+        pt.moveTo(new_audio[0], new_audio[1])
+        sleep(1)
+        pt.moveRel(230, 0)
+        pt.click()
+        sleep(1)
+        dl = pt.locateCenterOnScreen("download.png", confidence=.8)
+        if dl is not None:
+            pt.moveTo(dl[0], dl[1])
+            pt.click()
+            start_point()
+        else:
+            print("não achei botão de download")
+            pass
+
+
+
+def check_new_messages():    
     while True:
+        green_button = pt.locateOnScreen("new.png", confidence=.8)
         try: 
-            pt.moveTo(green_button)
-            pt.moveRel(-100,0)
-            pt.click()
-            sleep(1)
-            pt.screenshot(time_stamp)
-            pt.moveRel(-100, 200)
-            pt.click()
-            
-                        
-        
-            play_button = pt.locateOnScreen("play.png", confidence=.8 )
-        
-            print("tem audio")
-            pt.moveTo(play_button)
-            pt.moveRel(265, -30)
-            pt.click()
-            pt.moveRel(0, -180)
-            sleep(1)
-            pt.click()
-
-            sleep(1)
-            pt.moveRel(0, 180)
-            sleep(1)
-            pt.click()
-            sleep(1)
-
-            pt.moveRel(10, -45)
-            sleep(1)
-            pt.click()
-
-            sleep(1)
-            pt.moveRel(0, -50)
-            pt.click()
-            sleep(1)
-
-            sleep(1)
-            pt.moveRel(-400, 0)
-            pt.click()
-            sleep(1)
-
+            #detect the green circle indicating new message on screen
+            if green_button is not None:
+            #click on new message
+                pt.moveTo(green_button)
+                pt.moveRel(-100,0)
+                pt.click()
+                pt.screenshot(file_name)
+                sleep(.5)
+                try:
+                    dl_audio()
+                except:
+                    print("no audio sent")
         except:
+            start_point()
             print("no new messages")
 
 
